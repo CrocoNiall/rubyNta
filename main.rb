00d7e13e5aa1ba4e1eebd5c220804c7b1f705a4a
Niall Wallace <nj.wallace@outlook.com>
2015-12-10 15:19:59 +0000

    updated output

--- main.rb
+++ main.rb
@@ -27,7 +27,7 @@ elsif current_line != destination_line
   puts distance_to_intersect
   distance_from_intersect = (nta[destination_line].index(intersect).to_i - nta[destination_line].index(end_station).to_i).abs
   
-  puts "You need to take the " + current_line.to_s + " for " + distance_to_intersect.to_s + " stops. Change at " + intersect + " . Then take the " + destination_line.to_s + " line and get off after " + distance_from_intersect.to_s + " stops."
+  puts "You need to take the " + current_line.to_s + " Line for " + distance_to_intersect.to_s + " stops. Change at " + intersect + " . Then take the " + destination_line.to_s + " Line and get off after " + distance_from_intersect.to_s + " stops."
   puts "*****************************************************************************************************"
 end
 
